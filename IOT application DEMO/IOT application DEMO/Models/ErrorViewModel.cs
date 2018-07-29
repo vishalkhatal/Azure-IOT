9ce008b72e868a22a7aebbcd78f9b1b66cd4412d
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-07-29 15:38:33 +0530

    Added src code

---
+++ IOT application DEMO/IOT application DEMO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IOT_application_DEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
