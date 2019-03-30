aeb9290e6c710ec6ef730b416df6960cc783c506
Vishal Khatal <vishal.khatal2206@gmail.com>
2019-03-30 09:33:09 +0530

    Added device twin program

---
+++ DeviceTwinDemo/DeviceTwinDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceTwinDemo
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "IOT hub connection string";
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddTagsAndQuery().Wait();
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+        public static async Task AddTagsAndQuery()
+        {
+            var twin = await registryManager.GetTwinAsync("MyDotnetDevice");
+            var patch =
+                @"{
+             tags: {
+                 location: {
+                     region: 'India',
+                     plant: 'Hyderabad'
+                 }
+             }
+         }";
+            await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
+
+            var query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.plant = 'Hyderabad'", 100);
+            var twinsInHyderabad = await query.GetNextAsTwinAsync();
+            Console.WriteLine("Devices in Hyderabad: {0}", string.Join(", ", twinsInHyderabad.Select(t => t.DeviceId)));
+          }
+    }
+}
